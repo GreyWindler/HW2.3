70cfcf87af3e1dd3381ef265a216c3f8f872e5e8
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-09-17 23:05:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-
+import json
+from pprint import pprint
+with open('newsafr.json') as js_news:
+  
+  word = json.load(js_news)
+  
+  
+import xml.etree.elementtree as ET
+tree = ET.parse('newsafr.xml')
