f433c33c12e0ca9f80eb85f730a935493c0ee8b0
GreyWindler <43067710+GreyWindler@users.noreply.github.com>
2018-09-17 23:39:43 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import json
-from pprint import pprint
-with open('newsafr.json') as js_news:
-  
-  word = json.load(js_news)
+f=open('/home/mmstepanov/git/py-homeworks-new/2.3.formats.json.xml/files/newsafr.json')
+f=f.read()
+json_loads = json.loads(f)
+uniq_word=[]
+for i in json_loads['rss']['channel']['items']:
+	for word in i['description'].split(" "):
+		if word not in uniq_word and len(word)>6:
+			uniq_word.append(word)
   
   
 import xml.etree.elementtree as ET
